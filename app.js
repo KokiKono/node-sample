597545793b8c95bbf7db98831ce97c96ed4dc1ee
kokikono <kk.k.0421@outlook.jp>
2017-10-19 20:02:52 +0900

    socket

--- app.js
+++ app.js
@@ -2,13 +2,26 @@
  * Created by kokikono on 2017/10/19.
  */
 import express from 'express';
+import http from 'http';
+import SocketIO from 'socket.io';
 
 const app = express();
+let server = http.Server(app);
+let io = new SocketIO(server);
+let port = process.env.PORT || 3000;
 
 app.get('/',(req, res) => {
-    res.send('Hello World hogehoge');
+    res.sendFile(__dirname + '/index.html');
 });
 
-app.listen(3000, () => {
+
+io.on('connection', (socket) => {
+    console.log('a user connected');
+    socket.on('chat message', (msg) => {
+        console.log('message: ' + msg);
+    });
+});
+
+app.listen(port, () => {
     console.log('Example app listening on port 3000!');
 });
